d5be721e5804233c126ff3f1c4c41ea8e551eaa6
ms <mswain.db@gmail.com>
2026-09-12 10:01:43 +0000

    Prepare Flask backend for Render

--- app.py
+++ app.py
@@ -112,8 +112,10 @@ def book_service():
 if __name__ == "__main__":
     init_db()
 
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True
-    )
+import os
+
+app.run(
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 5000)),
+    debug=False
+)
